eb0659ae7ba35972732cf05e6dea0e664ae99396
winnie-26 <dnextone26@gmail.com>
2025-08-30 18:08:11 +0530

    Update constants.dart

--- frontend/lib/core/constants.dart
+++ frontend/lib/core/constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   static const String baseUrl = String.fromEnvironment(
     'BACKEND_BASE_URL',
-   // defaultValue: 'http://10.0.2.2:8080',
+   //defaultValue: 'http://10.0.2.2:8080',
    defaultValue: 'https://learningyogiassessment-3.onrender.com',
     
   );
